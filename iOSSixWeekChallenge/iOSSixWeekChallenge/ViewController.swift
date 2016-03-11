671313bb23c7f5b6822be1714956336976a868d1
Kaytee Cannon <kaytee.cannon@mac.com>
2016-03-11 09:10:27 -0700

    Created new project

---
+++ iOSSixWeekChallenge/iOSSixWeekChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSSixWeekChallenge
+//
+//  Created by Kaytee on 3/11/16.
+//  Copyright © 2016 Kaytee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
